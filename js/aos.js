f676bd77ff60811f623a7fb42985ab59ff6a4813
Franco Borri <francoborri11@gmail.com>
2025-10-16 16:49:05 -0300

    Proyecto finalizado

--- js/aos.js
+++ js/aos.js
@@ -1,26 +1,26 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Inicializar AOS
-    AOS.init({
-        duration: 1000,
-        once: true,
-        offset: 200
-    });
-    AOS.refresh();
+document.addEventListener("DOMContentLoaded", () => {
+  // Inicializar AOS
+  AOS.init({
+    duration: 1000,
+    once: true,
+    offset: 200,
+  });
+  AOS.refresh();
 
-    // Scroll suave para anclas con compensación del header
-    const headerOffset = 0; // ajustá al tamaño de tu navbar
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            e.preventDefault();
-            const target = document.querySelector(this.getAttribute('href'));
-            if (!target) return; // por si el id no existe
-            const elementPosition = target.offsetTop;
-            const offsetPosition = elementPosition - headerOffset;
+  // Scroll suave para anclas con compensación del header
+  const headerOffset = 0; // ajustá al tamaño de tu navbar
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const target = document.querySelector(this.getAttribute("href"));
+      if (!target) return; // por si el id no existe
+      const elementPosition = target.offsetTop;
+      const offsetPosition = elementPosition - headerOffset;
 
-            window.scrollTo({
-                top: offsetPosition,
-                behavior: "smooth"
-            });
-        });
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
     });
+  });
 });
--- js/map.js
+++ js/map.js
@@ -1,10 +1,14 @@
 function initMap() {
-    // Coordenadas de tu local
-    const ubicacion = { lat: -40.817340, lng: -63.010613, title: "Fabrica - Venezuela 218" };
+  // Coordenadas de tu local
+  const ubicacion = {
+    lat: -40.81734,
+    lng: -63.010613,
+    title: "Fabrica - Venezuela 218",
+  };
 
-    // Crear el mapa
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: ubicacion,
-    });
+  // Crear el mapa
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: ubicacion,
+  });
 }
